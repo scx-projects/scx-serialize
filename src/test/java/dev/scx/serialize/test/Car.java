43af8496a95dd8148c46f42fa65fb8b9ce9bed40
scx567888 <scx567888@outlook.com>
2026-07-24 09:56:17 +0800

    dev

--- src/test/java/dev/scx/serialize/test/Car.java
+++ src/test/java/dev/scx/serialize/test/Car.java
@@ -1,10 +1,20 @@
 package dev.scx.serialize.test;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Car {
 
     public String name;
     public LocalDateTime createdDate;
+
+    public AtomicLong mileage = new AtomicLong(18625);
+
+    public byte[] byteData = "Car Byte Data !!!".getBytes();
+
+    @Override
+    public String toString() {
+        return "name : " + name + "\ncreatedDate : " + createdDate+"\nmileage : " + mileage+"\nbyteData : " + new String(byteData);
+    }
 
 }
